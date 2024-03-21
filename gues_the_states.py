0f4b7f31a0edd54d097c4f877ce4365457e4b808
Natalija Prosic <162211449+redsparrow98@users.noreply.github.com>
2024-03-21 15:01:53 +0000

    Add files via upload
    Updated the code formatting to be in alignment to the Python PEP 8

--- gues_the_states.py
+++ gues_the_states.py
@@ -20,24 +20,24 @@ guessed_states = []
 #####################  Game  ######################
 
 while len(guessed_states) < 50:
-	answer_state = screen.textinput \
-		(title= f"{len(guessed_states)}/50 States Correct",\
-			prompt="What's another State name").title()
+    answer_state = screen.textinput \
+        (title= f"{len(guessed_states)}/50 States Correct",\
+            prompt="What's another State name").title()
 
-	#if the answer is exit close the game and save the states not
-	# guess in a list and convert it to a CSV file
-	if answer_state == "Exit":
-		missed_states = [state for state in all_states if state not in guessed_states]
-		new_data = pandas.DataFrame(missed_states)
-		new_data.to_csv("states_to_learn.csv")
-		break
+    #if the answer is exit close the game and save the states not
+    # guess in a list and convert it to a CSV file
+    if answer_state == "Exit":
+        missed_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missed_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
-	#check if the answer state is in the all states list and if it is write the state name on the map
-	if answer_state in all_states:
-		guessed_states.append(answer_state)
-		t = turtle.Turtle()
-		t.hideturtle()
-		t.penup()
-		state_data = data[data.state == answer_state]
-		t.goto(int(state_data.x), int(state_data.y))
-		t.write(answer_state)
+    #check if the answer state is in the all states list and if it is write the state name on the map
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
